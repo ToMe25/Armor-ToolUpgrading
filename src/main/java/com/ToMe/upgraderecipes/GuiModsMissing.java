e82f708d160a1ea789f6cd5b670bf401ce31ef02
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2018-08-26 09:58:31 +0200

    1.12.2
    Updated to 1.12.2-1.0

---
+++ src/main/java/com/ToMe/upgraderecipes/GuiModsMissing.java
@@ -0,0 +1,27 @@
+package com.ToMe.upgraderecipes;
+
+//import net.minecraftforge.fml.common.MissingModsException;
+
+//import net.minecraftforge.fml.common.MissingModsException;
+
+public class GuiModsMissing /*extends net.minecraftforge.fml.client.GuiModsMissing*/ {
+
+	/*public GuiModsMissing(MissingModsException modsMissing) {
+		super(modsMissing);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public GuiModsMissing(ModsMissingException modsMissing) {
+		super(null);
+		// TODO Auto-generated constructor stub
+	}*/
+	
+	/**
+	 * 
+	 * @param missing will throw an Exception if at least one String array Contains no loaded Modid.
+	 */
+	//public GuiModsMissing(String[]... missingModids) {
+		//super(null);
+	//}
+	
+}
